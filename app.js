be775f1249d01d83ab76a144fa7a4a0e04a7ca1c
ahmed altaweel <abohmaid77@gmail.com>
2024-02-13 11:14:18 +0300

    make it better

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname , '/public')));
 // app.use(express.static(__dirname + '/public'));
 
-
-app.listen(3000,function(){
+var port = process.env.PORT || 3000;
+app.listen(port,function(){
     console.log('server started');
 });
 
