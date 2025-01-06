92922fc05cc1db564cb1d218651f6d2c01eb70d5
ahmed altaweel <abohmaid77@gmail.com>
2025-01-06 11:25:05 +0300

    make it better

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const ejs = require('ejs');
 const path = require('path')
 var nodemailer = require('nodemailer');
 const bodyParser = require('body-parser');
+const {RecaptchaEnterpriseServiceClient} = require('@google-cloud/recaptcha-enterprise');
+
 
 
 const app = express();
@@ -108,3 +110,53 @@ app.post("/"  ,function async (req,res){
       res.redirect('/thanks');
 
 })
+
+
+async function createAssessment({
+  // TODO: Replace the token and reCAPTCHA action variables before running the sample.
+  projectID = "thrill-website-1736151402229",
+  recaptchaKey = "6LeecK8qAAAAAPPJQDmpQQhwkoHc95rbNRJcq1Ou",
+  token = "action-token",
+  recaptchaAction = "action-name",
+}) {
+  // Create the reCAPTCHA client.
+  // TODO: Cache the client generation code (recommended) or call client.close() before exiting the method.
+  const client = new RecaptchaEnterpriseServiceClient();
+  const projectPath = client.projectPath(projectID);
+
+  // Build the assessment request.
+  const request = ({
+    assessment: {
+      event: {
+        token: token,
+        siteKey: recaptchaKey,
+      },
+    },
+    parent: projectPath,
+  });
+
+  const [ response ] = await client.createAssessment(request);
+
+  // Check if the token is valid.
+  if (!response.tokenProperties.valid) {
+    console.log(`The CreateAssessment call failed because the token was: ${response.tokenProperties.invalidReason}`);
+    return null;
+  }
+
+  // Check if the expected action was executed.
+  // The `action` property is set by user client in the grecaptcha.enterprise.execute() method.
+  if (response.tokenProperties.action === recaptchaAction) {
+    // Get the risk score and the reason(s).
+    // For more information on interpreting the assessment, see:
+    // https://cloud.google.com/recaptcha-enterprise/docs/interpret-assessment
+    console.log(`The reCAPTCHA score is: ${response.riskAnalysis.score}`);
+    response.riskAnalysis.reasons.forEach((reason) => {
+      console.log(reason);
+    });
+
+    return response.riskAnalysis.score;
+  } else {
+    console.log("The action attribute in your reCAPTCHA tag does not match the action you are expecting to score");
+    return null;
+  }
+}
